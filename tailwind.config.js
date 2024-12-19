b6fdc6e26c4c9597b15b07cca981e3a9f3fcac82
Pedro Soto Soto <dex.pgsoto@gmail.com>
2024-12-19 01:08:08 +0000

    add theme sample

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
